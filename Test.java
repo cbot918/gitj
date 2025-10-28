53fb9a9bdaf4cbf0af558234a5706fce41cf2bec
cbot918 <cbot918@gmail.com>
2025-10-28 22:58:46 +0800

    task

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    
+    public void test(){
+        System.out.println("This is a test model1.");
+        System.out.println("This is a test m2.");
+        System.out.println("This is a test method3.");
+    }
+
+}
